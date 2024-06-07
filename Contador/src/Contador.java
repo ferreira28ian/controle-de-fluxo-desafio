04b6d379d33f82f86732fa9b4a5cd5dcb4a76635
Ian Ferreira <ianferreira_silva@hotmail.com>
2024-06-07 18:36:26 -0400

    desafio aperfeiçoado

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -1,21 +1,5 @@
-import java.util.Scanner;
-
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
-
-        try{
-            contar(parametroUm, parametroDois);    
-        }catch(ParametrosInvalidosException e) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-        }
-    }
-
-	static void contar (Integer parametroUm, Integer parametroDois) throws ParametrosInvalidosException {
+    static void contar (Integer parametroUm, Integer parametroDois) throws ParametrosInvalidosException {
         if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException();
         }
@@ -28,5 +12,5 @@ public class Contador {
             totalDeContagem = i;
         }
         System.out.println("Total de ocorências " + totalDeContagem);
-    }    
+    } 
 }
---
+++ Contador/src/DesafioControleFluxo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DesafioControleFluxo {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            Contador.contar(parametroUm, parametroDois);    
+        }catch(ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }   
+}
